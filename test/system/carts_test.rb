8367e9e568963b5a516df8c4004310977b6b7a1e
Will Olson <will.olson11@gmail.com>
2018-05-08 20:58:23 -0700

    Entering additional payment details

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -1,9 +1,37 @@
 require "application_system_test_case"
 
 class CartsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit carts_url
-  #
-  #   assert_selector "h1", text: "Cart"
-  # end
+  test "cart is revealed when it has items" do
+    visit store_index_url
+
+    assert_no_selector 'h2', text: 'Your Cart'
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    assert_selector 'h2', text: 'Your Cart'
+  end
+
+  test "cart is hidden when it has no items" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    assert_selector 'h2', text: 'Your Cart'
+
+    accept_alert do
+      click_on 'Empty cart'
+    end
+
+    assert_no_selector 'h2', text: 'Your Cart'
+  end
+
+  test "product is highlighted when added to the cart" do
+    visit store_index_url
+
+    assert_no_selector 'tr.line-item-highlight'
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    assert_selector 'tr.line-item-highlight'
+  end
 end
