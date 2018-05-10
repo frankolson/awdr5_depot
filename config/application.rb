6f8c83bfa70c66f192f65421b5c07ed95e8cdfa5
Will Olson <will.olson11@gmail.com>
2018-05-09 20:14:56 -0700

    Processing Emails and Payments Efficiently

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module Depot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.delivery_method = :sendmail
   end
 end
