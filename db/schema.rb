ff074983c3557748e1071f2f4c92d627a659e2bf
Will Olson <will.olson11@gmail.com>
2018-05-11 14:02:01 -0700

    Logging in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180428213428) do
+ActiveRecord::Schema.define(version: 20180510202743) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 20180428213428) do
     t.string "name"
     t.text "address"
     t.string "email"
-    t.integer "pay_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "pay_type"
   end
 
   create_table "products", force: :cascade do |t|
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20180428213428) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
